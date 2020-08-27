fccdb5c55670da62a545cde088f1b07cb81a7f71
Dwarikanath1502 <68079395+Dwarikanath1502@users.noreply.github.com>
2020-08-27 23:15:03 +0530

    Added Environment Variable to databse

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,16 +1,19 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/tshirt' , {
+mongoose
+    .connect(process.env.DATABASE , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-})
- .then(()=>{
-     console.log("DB CONNECTED...");
- })
-const port = 8000;
+    })
+    .then(()=>{
+         console.log("DB CONNECTED...");
+    })
+const port = process.env.PORT || 8000;
 app.listen(port, ()=> {
     console.log(`App is running at port: ${port} `);
 }); 

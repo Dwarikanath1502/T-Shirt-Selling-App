2b80b8c0797588a3f52b48fd3b512d6d9846fca0
Dwarikanath1502 <68079395+Dwarikanath1502@users.noreply.github.com>
2020-08-27 18:31:14 +0530

    Completed UserSchema

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const crypto  = require('crypto');
+const uuidv1 = require('uuid/v1');
 
 var userSchema = new mongoose.Schema({
     name: {
@@ -39,12 +41,29 @@ var userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.virtual("password")
+    .set(function(password){
+        this._password = password
+        this.salt = uuidv1();
+        this.encry_password = this.securePassword(password);
+    })
+    .get(function(){
+        return this._password
+    })
+
+
 userSchema.method = {
+
+    authenticate: function(plainPassword){
+        return this.securePassword(plainPassword) === this.encry_password
+    },
+    
     securePassword: function(plainPassword){
         if(!password)
             return "";
         try {
-            return createImageBitmap.createHmac('sha256', this.salt)
+            return crypto
+            .createHmac('sha256', this.salt)
             .update(plainPassword)
             .digest('hex');
         } catch (err) {

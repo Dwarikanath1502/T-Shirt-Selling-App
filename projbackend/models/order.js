e95ca029cb73908deda82e20647e6b40fbbaf3d8
Dwarikanath1502 <68079395+Dwarikanath1502@users.noreply.github.com>
2020-08-27 19:03:20 +0530

    Updated OrderSchema

---
+++ projbackend/models/order.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema;
+
+const ProductCartSchema = new mongoose.Schema({
+        product:{
+            type: ObjectId,
+            ref: "User"
+        },
+        name: String,
+        count: Number,
+        price: Number
+        
+    })
+const ProductCart = mongoose.model("ProductCart",orderSchema)
+
+const orderSchema = new mongoose.Schema({
+    products: [ProductCartSchema],
+    transaction_id: {},
+    amount: {type: Number},
+    address: String,
+    updated: Date,
+    user: {
+        type: ObjectId,
+        ref: "User"
+    }
+},
+{timestamps: true}
+); 
+
+const Order = mongoose.model("Order",orderSchema)
+
+module.exports = {Order, ProductCart}

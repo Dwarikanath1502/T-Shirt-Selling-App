6cdd164730154614d1561a293c68edb6c8d3b264
Dwarikanath1502 <68079395+Dwarikanath1502@users.noreply.github.com>
2020-08-28 17:46:42 +0530

    Updated Controllers and routes

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -8,7 +8,7 @@ const {signout , signup} = require("../controllers/auth")
 router.post("/signup",[
     check("name").isLength({ min: 3 }).withMessage("Name should be 3 chars long"),
     check("email" , "Email is required").isEmail(),
-    check("password" , "password should be atleast 5 char").isLength({min: 5}),
+    check("password" , "password should be atleast 5 char").isLength({min: 5})  ,
 ], signup);
 router.get("/signout" , signout)
 

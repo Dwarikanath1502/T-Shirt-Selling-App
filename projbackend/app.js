81133d53140853d4016b7d1499696d33b2c3c751
Dwarikanath1502 <68079395+Dwarikanath1502@users.noreply.github.com>
2020-08-28 12:38:17 +0530

    Middleware Intacked

--- projbackend/app.js
+++ projbackend/app.js
@@ -3,16 +3,25 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
 
 mongoose
-    .connect(process.env.DATABASE , {
+    .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
     })
     .then(()=>{
-         console.log("DB CONNECTED...");
+         console.log("DB CONNECTED..."); 
     })
+
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    app.use(cors());
+
 const port = process.env.PORT || 8000;
 app.listen(port, ()=> {
     console.log(`App is running at port: ${port} `);

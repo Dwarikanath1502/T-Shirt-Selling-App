44bd94f42ab5c6be7cc7253bdcc74df40d23aa92
Dwarikanath1502 <68079395+Dwarikanath1502@users.noreply.github.com>
2020-08-28 13:01:25 +0530

    Authentication route created

--- projbackend/app.js
+++ projbackend/app.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
+const authRoutes = require("./routes/auth");
 
+//DB CONNECTION
 mongoose
     .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -17,12 +19,19 @@ mongoose
     .then(()=>{
          console.log("DB CONNECTED..."); 
     })
-
+//MIDDLEWARES
     app.use(bodyParser.json());
     app.use(cookieParser());
     app.use(cors());
 
+//MY ROUTES
+    app.use("/api", authRoutes);
+
+
+//PORTS
 const port = process.env.PORT || 8000;
+
+//STARTING A SERVER
 app.listen(port, ()=> {
     console.log(`App is running at port: ${port} `);
 }); 
---
+++ projbackend/routes/auth.js
@@ -0,0 +1,10 @@
+
+var express = require("express")
+var router = express.Router()
+
+
+router.get("/signout" , (req , res)=>{
+    res.send("User Signout")
+})
+
+module.exports = router;

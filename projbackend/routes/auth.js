ff5868d8c217df12a0bf42e08bcc46b761283afa
Dwarikanath1502 <68079395+Dwarikanath1502@users.noreply.github.com>
2020-08-28 17:59:26 +0530

    Created signin route

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,14 +2,26 @@ var express = require("express")
 var router = express.Router()
 const { check , validationResult} = require('express-validator');
 
-const {signout , signup} = require("../controllers/auth")
+const {signout , signup , signin} = require("../controllers/auth")
 
 
-router.post("/signup",[
+router.post(
+    "/signup",[
     check("name").isLength({ min: 3 }).withMessage("Name should be 3 chars long"),
     check("email" , "Email is required").isEmail(),
     check("password" , "password should be atleast 5 char").isLength({min: 5})  ,
-], signup);
+], signup
+);
+
+router.post(
+    "/signin",[
+    check("email" , "Email is required").isEmail(),
+    check("password" , "Password is Required!").isLength({min: 1})  ,
+], 
+signin
+);
+
+
 router.get("/signout" , signout)
 
 module.exports = router;

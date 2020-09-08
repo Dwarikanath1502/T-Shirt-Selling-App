ed13f5b04c78d0be99ac93f33ad49fdd0224b7d2
Dwarikanath1502 <68079395+Dwarikanath1502@users.noreply.github.com>
2020-09-08 16:09:26 +0530

    Fixed sending Token in cookie for user bug

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,37 +1,37 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const mongoose = require('mongoose');
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
 
-//DB CONNECTION
+//DB Connection
 mongoose
-    .connect(process.env.DATABASE, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-    })
-    .then(()=>{
-         console.log("DB CONNECTED..."); 
-    })
-//MIDDLEWARES
-    app.use(bodyParser.json());
-    app.use(cookieParser());
-    app.use(cors());
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
 
-//MY ROUTES
-    app.use("/api", authRoutes);
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
+//My Routes
+app.use("/api", authRoutes);
 
-//PORTS
+//PORT
 const port = process.env.PORT || 8000;
 
-//STARTING A SERVER
-app.listen(port, ()=> {
-    console.log(`App is running at port: ${port} `);
-}); 
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
